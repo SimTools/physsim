830a241d906c9c808660be2f2ddf1cf7db433dc7
ikematsu <>
2001-07-09 14:03:56 +0000

    2001/07/09 K.Ikematsu Very minor update

--- Anlib/src/ANL3DVector.cxx
+++ Anlib/src/ANL3DVector.cxx
@@ -6,16 +6,19 @@
 //* (Description)
 //*    A very primitive lockable 3D vector class.
 //* (Requires)
-//* 	class Lockable
 //*	class TVector3
+//* 	class Lockable
+//* 	class ANL2DVector
 //* (Provides)
 //* 	class ANL3DVector
 //* (Update Recored)
 //*    1999/09/13  K.Ikematsu	Original version.
-//*    2000/03/23  K.Ikematsu   Added GetNorm method.
-//*    2000/03/23  K.Ikematsu   Added GetTheta method.
-//*    2000/03/28  K.Ikematsu   Added Acol method.
+//*    2000/03/23  K.Ikematsu	Added GetNorm method.
+//*    2000/03/23  K.Ikematsu	Added GetTheta method.
+//*    2000/03/28  K.Ikematsu	Added Acol method.
+//*    2001/02/16  K.Ikematsu	Added GetTrans and GetLong method.
 //*
+//* $Id$
 //*************************************************************************
 //
 #include "ANL3DVector.h"
--- Anlib/src/ANL4DVector.cxx
+++ Anlib/src/ANL4DVector.cxx
@@ -6,16 +6,18 @@
 //* (Description)
 //*    A very primitive lockable Lorentz vector class.
 //* (Requires)
-//* 	class Lockable
 //*	class TLorentzVector
+//* 	class Lockable
+//*     class ANL3DVector
 //* (Provides)
 //* 	class ANL4DVector
 //* (Update Recored)
 //*    1999/09/05  K.Ikematsu	Original version.
-//*    2000/03/23  K.Ikematsu   Added Get3D method.
-//*    2000/03/23  K.Ikematsu   Added GetTheta method.
-//*    2000/03/28  K.Ikematsu   Added Acol method.
+//*    2000/03/23  K.Ikematsu	Added Get3D method.
+//*    2000/03/23  K.Ikematsu	Added GetTheta method.
+//*    2000/03/28  K.Ikematsu	Added Acol method.
 //*
+//* $Id$
 //*************************************************************************
 //
 #include "ANL4DVector.h"
