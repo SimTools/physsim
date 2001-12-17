4f8be6a5ca6d10f62c26121ba8163f989bc5a7db
ikematsu <>
2001-12-17 19:36:53 +0000

    2001/12/17 K.Ikematsu  Added ANLCheatedJadeJetFinder, ANLCheatedJadeEJetFinder and ANLCheatedDurhamJetFinder classes and Removed #ifdef __DEBUG__

--- Anlib/src/ANLCheatedJetFinder.h
+++ Anlib/src/ANLCheatedJetFinder.h
@@ -1,8 +1,40 @@
 #ifndef __ANLCHEATEDJETFINDER__
 #define __ANLCHEATEDJETFINDER__
 //*************************************************************************
-//* $Id$
+//* =============================
+//*  ANLCheatedJetFinder Classes
+//* =============================
 //*
+//* (Description)
+//*    Jet finder using generator information classes for JLC analysis
+//* (Requires)
+//*     class ANLTrack
+//*     class ANLJet
+//*     class ANLJetFinder
+//* (Provides)
+//*     class ANLTaggedJet
+//*     class ANLCheatedJetFinder
+//*     class ANLCheatedJadeJetFinder
+//*     class ANLCheatedJadeEJetFinder
+//*     class ANLCheatedDurhamJetFinder
+//* (Usage)
+//*     // Example
+//*     Double_t ycut = 0.01;
+//*     ANLCheatedJadeEJetFinder jclust(ycut);
+//*     TIter nexttrk(&tracks);
+//*     while ((trkp = (ANLTrack *)nexttrk())) {
+//*       trkp->SetColorSingletID();
+//*     }
+//*     jclust.Initialize(tracks); // tracks: TObjArray of LVector derivatives.
+//*     jclust.FindJets();         // finds jets with ycut = 0.01.
+//*     Int_t njets = 2;           // One can also force the event to be
+//*     jclust.ForceNJets(njets);  // "njets" jets.
+//* (Update Recored)
+//*    2001/10/22  K.Ikematsu   Original version
+//*
+//* $Id$
+//*************************************************************************
+//
 #include "ANLTrack.h"
 #include "ANLJetFinder.h"
 //_____________________________________________________________________
@@ -37,9 +69,7 @@ class ANLCheatedJetFinder : public ANLJetFinder {
 public:
    ANLCheatedJetFinder(Double_t y = 0.) : ANLJetFinder(y) {}  // default constructor
    ANLCheatedJetFinder(const ANLCheatedJetFinder &jf) : ANLJetFinder(jf) {
-#ifdef __DEBUG__
-     cerr << "  copy constructor is called ..." << endl;
-#endif
+
      ANLTaggedJet *jetsrc;
      ANLTaggedJet *jet;
      TIter nextsrc( & (((ANLCheatedJetFinder &)jf).GetJets()));
@@ -47,9 +77,6 @@ public:
      while ((jet = (ANLTaggedJet *)next())) {
        jetsrc = (ANLTaggedJet *)nextsrc();
        jet->SetTag(jetsrc->GetTag());
-#ifdef __DEBUG__
-       cerr << "  fTag = " << jet->GetTag() << endl;
-#endif
      }
    }
 
@@ -57,18 +84,56 @@ public:
      return new ANLTaggedJet();
    }
    ANLJetFinder *NewJetFinder(ANLJetFinder *jf) {
-#ifdef __DEBUG__
-     cerr << "ANLCheatedJetFinder::NewJetFinder() is called ..." << endl;
-#endif
      return new ANLCheatedJetFinder(*(ANLCheatedJetFinder *)jf);
                                     // make new ANLCheatedJetFinder object
    }
 
+   virtual Double_t GetYmass(const ANL4DVector &p1,
+			     const ANL4DVector &p2) const;
 
+   ClassDef(ANLCheatedJetFinder,1)  // Cheated Jet Finder class
+};
+
+//_____________________________________________________________________
+//  -----------------------------
+//  ANLCheatedJadeJetFinder Class
+//  -----------------------------
+//
+class ANLCheatedJadeJetFinder : public ANLCheatedJetFinder {
+public:
+   ANLCheatedJadeJetFinder(Double_t y = 0.) : ANLCheatedJetFinder(y) {}  // default constructor
 
    Double_t GetYmass(const ANL4DVector &p1, const ANL4DVector &p2) const;
 
-   ClassDef(ANLCheatedJetFinder,1)  // Cheated Jet Finder class
+   ClassDef(ANLCheatedJadeJetFinder,1)  // Cheated Jade Jet Finder class
+};
+
+//_____________________________________________________________________
+//  ------------------------------
+//  ANLCheatedJadeEJetFinder Class
+//  ------------------------------
+//
+class ANLCheatedJadeEJetFinder : public ANLCheatedJetFinder {
+public:
+   ANLCheatedJadeEJetFinder(Double_t y = 0.) : ANLCheatedJetFinder(y) {}  // default constructor
+
+   Double_t GetYmass(const ANL4DVector &p1, const ANL4DVector &p2) const;
+
+   ClassDef(ANLCheatedJadeEJetFinder,1)  // Cheated Jade EJet Finder class
+};
+
+//_____________________________________________________________________
+//  -------------------------------
+//  ANLCheatedDurhamJetFinder Class
+//  -------------------------------
+//
+class ANLCheatedDurhamJetFinder : public ANLCheatedJetFinder {
+public:
+   ANLCheatedDurhamJetFinder(Double_t y = 0.) : ANLCheatedJetFinder(y) {}  // default constructor
+
+   Double_t GetYmass(const ANL4DVector &p1, const ANL4DVector &p2) const;
+
+   ClassDef(ANLCheatedDurhamJetFinder,1)  // Cheated Durham Jet Finder class
 };
 
 #endif
