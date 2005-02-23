375f21b9ed8d085944d98f56f183394266116bef
Fujii Keisuke <keisuke.fujii@kek.jp>
2005-02-23 12:58:32 +0000

    2005/02/23 K.Fujii

--- Anlib/examples/jsf/tt/l+4jet/anlL4J.C
+++ Anlib/examples/jsf/tt/l+4jet/anlL4J.C
@@ -21,31 +21,20 @@ int anlL4J()
   TFile *fin;
   JSFSteer *jsf;
 
+  jsf  = new JSFSteer();			// Create JSF object
+
   Char_t *outputfile="jsf.root";  // A file to output histograms
   Char_t *inputfile="../../../../../top/TTStudy/prod/ttsim.root";
   // Char_t *inputfile="ttsim.root";	// Input simulator file.
 
-  if( strncmp(gSystem->HostName(),"ccjlc",5)  != 0 ) {
-    if( strncmp(gSystem->Getenv("OSTYPE"),"hpux",4) ==0 ) {
-      gSystem->Load("libS4Utils.sl");
-      gSystem->Load("libAnlib.sl");
-      gSystem->Load("libJSFAnlib.sl");
-      gSystem->Load("../../../../../top/TTStudy/prod/TTSpring.sl");
-      gSystem->Load("libTTL4JAnalysis.sl");
-    }
-    else {
       gSystem->Load("libS4Utils.so");
       gSystem->Load("libAnlib.so");
       gSystem->Load("libJSFAnlib.so");
       gSystem->Load("../../../../../top/TTStudy/prod/TTSpring.so");
       gSystem->Load("libTTL4JAnalysis.so");
-   }
-  }
 
   file = new TFile(outputfile,"RECREATE");  	// Output file
   fin  = new TFile(inputfile);            	// Input simulator data
-
-  jsf  = new JSFSteer();			// Create JSF object
   jsf->SetInput(*fin);
   jsf->SetOutput(*file);
 
