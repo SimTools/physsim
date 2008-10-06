ab79a4f2191227b436560d74feffe5822a2467fa
Fujii Keisuke <keisuke.fujii@kek.jp>
2008-10-06 07:56:24 +0000

    2008/10/06 K.Fujii

--- top/TTStudy/prod/nsim.C
+++ top/TTStudy/prod/nsim.C
@@ -1,26 +1,31 @@
 { gROOT->Reset();
-  TFile file("ttsim.root","RECREATE");  // Output file
+  TFile file("ttsim02.root","RECREATE");  // Output file
  
   jsf    = new JSFSteer();
   full   = new JSFLCFULL();
- 
   spring = new TTSpring("TTSpring");
-  spring->ReadBases("bases.root");
  
-  printf(" Roots is %g\n",((TTBases*)spring->GetBases())->GetRoots());
+  TFile flast("ttsim.root","READ");
+  jsf->GetLastRunInfo(&flast);
+  flast.Close();
 
   hdr=new JSFHadronizer();
   sim=new JSFQuickSim();
+
+  spring->ReadBases("bases.root");
+
+  Int_t maxevt=1000;      // Number of events.
+  jsf->Initialize();
+
+
+  printf(" Roots is %g\n",((TTBases*)spring->GetBases())->GetRoots());
   
 //  TTree::SetBranchStyle(0);
 //  full->SetMakeBranch(kFALSE);   // suppress output of EventBuf 
 //  hdr->SetMakeBranch(kFALSE);    // suppress output of EventBuf 
 //  sim->SetMakeBranch(kFALSE);    // suppress output of EventBuf
 
-  Int_t maxevt=1000;      // Number of events.
-  jsf->Initialize();
-
-  jsf->BeginRun(30);      // Set run number to 30.
+  jsf->BeginRun(31);      // Set run number to 30.
   for(Int_t ev=1;ev<=maxevt;ev++){
     printf(" start event %d\n",ev);
     if( !jsf->Process(ev) ) break;
@@ -32,5 +37,4 @@
   file->Write();
 
 }
-
 
