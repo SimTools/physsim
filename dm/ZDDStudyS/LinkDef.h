dd115bae315c55b22bd03cde9e48b8285d80d9e9
Fujii Keisuke <keisuke.fujii@kek.jp>
2009-05-21 06:06:39 +0000

    2009/05/21 T.Honda & K.Fujii for Higgs portal scenario (DM=Scalar)

---
+++ dm/ZDDStudyS/LinkDef.h
@@ -0,0 +1,11 @@
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ class ZDDSpring+;
+#pragma link C++ class ZDDSpringBuf+;
+#pragma link C++ class ZDDBases+;
+
+#endif
