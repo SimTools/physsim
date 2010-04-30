53323260f9ae5d7cba7496a991969151419a3959
Fujii Keisuke <keisuke.fujii@kek.jp>
2010-04-30 08:48:53 +0000

    2010/04/30 K.Fujii

--- higgs/WWHStudy++/prod/xsection.sh
+++ higgs/WWHStudy++/prod/xsection.sh
@@ -7,7 +7,7 @@ for ((npt=0; $npt != 4; npt=$npt+1))
 do 
   jsf -b -q xsection.C\($((400+50*$npt))\)
 done
-for ((npt=0; $npt != 5; npt=$npt+1))
+for ((npt=0; $npt != 10; npt=$npt+1))
 do 
   jsf -b -q xsection.C\($((600+100*$npt))\)
 done
