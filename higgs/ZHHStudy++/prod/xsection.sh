5d1b6b82a11c2943bc4fb9a9ff6cbbef91d07b7c
Fujii Keisuke <keisuke.fujii@kek.jp>
2010-04-30 08:54:53 +0000

    2010/04/30 K.Fujii

--- higgs/ZHHStudy++/prod/xsection.sh
+++ higgs/ZHHStudy++/prod/xsection.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-for ((npt=0; $npt != 35; npt=$npt+1))
+for ((npt=0; $npt != 4; npt=$npt+1))
 do 
   jsf -b -q xsection.C\($((310+10*$npt))\)
+done
+for ((npt=0; $npt != 15; npt=$npt+1))
+do 
+  jsf -b -q xsection.C\($((400+20*$npt))\)
 done
 for ((npt=0; $npt != 9; npt=$npt+1))
 do 
